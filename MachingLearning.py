40eb900b99887a07520773cffc078d88c9869205
soohyunkyung <113413847+soohyunkyung@users.noreply.github.com>
2024-04-16 18:27:26 +0900

    Update MachingLearning.py

--- MachingLearning.py
+++ MachingLearning.py
@@ -1,12 +1,14 @@
 from sklearn.neighbors import KNeighborsClassifier
-kn=KNeighborsClassifier()
-kn.fit(WGS84LON, WGS84LAT) #드롭다운 된 경도, 위도를 가져와야 합니다.
-user=[경도, 위도] #사용자의 경도, 위도입니다.
-kn.predict([new]) 
-distances, indexes=kn.neighbors([new])
 
-for i in indexes:
-  print(DUTYNAME[i], DUTYADDR[i]) #사용자 주변 5개의 약국 이름, 약국 주소를 출력합니다.
+def get_pharmacy(longitude, latitude)
+  kn=KNeighborsClassifier()
+  kn.fit(longitude, latitude) #드롭다운 된 경도, 위도를 가져와야 합니다.
+  user=[경도, 위도] #사용자의 경도, 위도입니다.
+  kn.predict([new]) 
+  distances, indexes=kn.neighbors([new])
 
-for i in len(indexes):
-  print(distances[i]) #거리 변환이 필요합니다.
+  for i in indexes:
+    print(name[i], gu[i]+" "+road_name_adress[i]) #사용자 주변 5개의 약국 이름, 약국 주소를 출력합니다.
+
+  for i in len(indexes):
+    print(distances[i]) #거리 변환이 필요합니다.
